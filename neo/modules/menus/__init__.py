e33cb79f7c54c90054b0a2e239f9f2135867b700
nickofolas <nickofolas@gmail.com>
2021-07-08 17:33:22 -0500

    Rework menus

---
+++ neo/modules/menus/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2021 nickofolas
+from .menus import ButtonsMenu, DropdownMenu
+from .pages import EmbedPages, Pages
--- neo/modules/paginator/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2021 nickofolas
-from .pages import Pages
-from .paginator import Paginator
